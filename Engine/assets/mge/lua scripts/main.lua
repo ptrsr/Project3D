b9d1adb56141cded9303f6ef4ce1d3e3457e1b8a
Stand <stanvmutafchiev@gmail.com>
2017-02-14 12:00:02 +0100

    Exporter updated to work with empty gameobjects from unity(only transform is exported)

--- Engine/assets/mge/lua scripts/main.lua
+++ Engine/assets/mge/lua scripts/main.lua
@@ -4,4 +4,4 @@ CubeONE = GameObject:new{name = "CubeONE",mesh = "cube_flat.obj",x =-2.94,y = -0
 CubeTWO = GameObject:new{name = "CubeTWO",mesh = "cube_flat.obj",x =-5.51,y = -0.5,z =-2.89,r= 1,g = 1,b = 1, rotationX = 324.5535, rotationY = 130.5601, rotationZ = 0.0000}
 CubeTHREE = GameObject:new{name = "CubeTHREE",mesh = "cube_flat.obj",x =-5.35,y = -0.5,z =-5.49,r= 1,g = 1,b = 1, rotationX = 0.0000, rotationY = 130.5601, rotationZ = -36.4190}
 CubeFOUR = GameObject:new{name = "CubeFOUR",mesh = "cube_flat.obj",x =-2.5,y = -0.5,z =-2.16,r= 1,g = 1,b = 1, rotationX = 0.0000, rotationY = -174.4500, rotationZ = 0.0000}
-Plane = GameObject:new{name = "Plane",mesh = "cube_flat.obj",x =-4.59,y = 1.33,z =-11.56,r= 1,g = 0,b = 0, rotationX = 17.8653, rotationY = -180.0000, rotationZ = 0.0000}
+Plane = GameObject:new{name = "Plane",x =-4.59,y = 1.33,z =-11.56, rotationX = 17.8653, rotationY = -180.0000, rotationZ = 0.0000}
